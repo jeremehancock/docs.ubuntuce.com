257192596c3743489b58b6061bdf9ab3899a7fba
Client <michael@jeremehancock.com>
2021-08-13 04:46:29 +0000

    update

--- bl-themes/docs-x-3.2/init.php
+++ bl-themes/docs-x-3.2/init.php
@@ -1,5 +1,5 @@
 <?php
-$GITHUB_BASE_URL = 'https://github.com/mhancoc7/docs.ubuntuce.com-content/blob/main/pages/';
+$GITHUB_BASE_URL = 'https://github.com/jeremehancock/docs.ubuntuce.com-content/blob/main/pages/';
 $FILENAME = 'index.md';
 
 $REDIRECT_PARENT_TO_FIRST_CHILD = true;
