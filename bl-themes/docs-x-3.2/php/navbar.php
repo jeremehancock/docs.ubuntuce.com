d3819c20bbf8c2b927eaa80cd8aad35389dd478c
Jereme <michael@jeremehancock.com>
2022-05-12 22:55:59 -0500

    updated nav

--- bl-themes/docs-x-3.2/php/navbar.php
+++ bl-themes/docs-x-3.2/php/navbar.php
@@ -1,4 +1,4 @@
-<nav class="navbar fixed-top navbar-expand-md navbar-dark bg-dark">
+<nav class="navbar fixed-top navbar-expand-md navbar-dark bg-dark" style="padding: 0.5rem 0 0.5rem;">
 	<div class="container-fluid" style="justify-content: flex-start;">
 	    <div class="b-burger-btn"><i></i><b></b></div>
 		<a class="navbar-brand" href="<?php echo $site->url() ?>">
